daaaa708e45abcc875938c7b82a358158c108828
KaungSackNaing <kaungsack1512@gmail.com>
2024-10-12 03:11:53 +0630

    Initial Commit

---
+++ recipe/recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeApp.swift
+//  recipe
+//
+//  Created by Kaung Sett Naing on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
